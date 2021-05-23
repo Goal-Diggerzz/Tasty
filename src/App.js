eef995a4e6d840e194e2088491fcf3b2ab39021e
Yousef27h <yousef_salem97@yahoo.com>
2021-05-23 12:36:23 +0300

    footer and recipe body

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Footer from "./components/footer";
+import Recipes from "./components/recipes";
+import ModalApp from "./components/modalApp";
+import axios from 'axios';
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
+  getRecipesData = async() =>{
+    const queries = {
+
+    }
+    const url = `${REACT_APP_SERVER}/recipes`
+    const recipesData = 
+  }
+  showModalFunc = () => {
+    this.setState({
+      showModal: true,
+    });
+  };
+  closeModal = () => {
+    this.setState({
+      showModal: false,
+    });
+  };
   render() {
     return (
       <div>
         <h1>Hi</h1>
+        <Recipes showModalFunc={this.showModalFunc} />
+        <ModalApp closeModal={this.closeModal} showModal={this.state.showModal} />
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
