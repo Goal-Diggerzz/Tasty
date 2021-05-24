b4950b1936177dc959cbfb90e93189c2280c6d27
murad alshorman <muradalshorman@gmail.com>
2021-05-24 13:16:37 +0300

    about us

--- src/App.js
+++ src/App.js
@@ -3,13 +3,42 @@
 import React, { Component } from 'react';
 import Home from './components/homePage';
 import Header from './components/header';
+import Aboutus from './components/AboutUs';
 export class App extends Component {
   render() {
     return (
       <>
-   <Header/>
-        <Home />
-</>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home
+                updateRender={this.updateRender}
+                showCards={this.state.showCards}
+              />
+            </Route>
+            <Route exact path="/recipes">
+              <Recipes foodData={this.state.recipiesData}
+              />
+              <Search
+                getRecipesData={this.getRecipesData}
+                updateQuery={this.updateQuery}
+              />
+            </Route>
+            <Route exact path="/blogs">
+              <Blogs />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Route exact path="/aboutus">
+              <AboutUs />
+            </Route>
+          </Switch>
+          <ModalApp closeModal={this.closeModal} showModal={this.state.showModal} />
+        </Router>
+        <Footer />
+      </>
     );
   }
 }
